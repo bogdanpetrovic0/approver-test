540d88f07d682a66786ec628df1fc48d29db14d2
Jaša Bartelj <bartmanus@users.noreply.github.com>
2022-12-02 15:15:45 +0100

    sync with UabProd Sat maintenance window [DEVOPS-1155]
    https://celtra.atlassian.net/browse/DEVOPS-1155

--- app/circleci_approver.py
+++ app/circleci_approver.py
@@ -94,7 +94,8 @@ class CircleciApprover:
 def main() -> None:
     scheduler = BlockingScheduler()
     approver = CircleciApprover()
-    scheduler.add_job(approver.fetch_and_approve_jobs, 'cron', hour='9,13,16', timezone='Europe/Ljubljana')
+    scheduler.add_job(approver.fetch_and_approve_jobs, 'cron', day_of_week='mon,tue,wed,thu,fri', hour='9,13,16', timezone='Europe/Ljubljana')
+    scheduler.add_job(approver.fetch_and_approve_jobs, 'cron', day_of_week='sat', hour='6', timezone='UTC')
     scheduler.start()
 
 if __name__ == "__main__":
